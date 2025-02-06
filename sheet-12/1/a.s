609e62dc15a316e55317e6802136307d01e4d898
mdan22 <mdan22@proton.me>
2025-02-06 11:19:00 +0100

    optimize 12-1-a

--- sheet-12/1/a.s
+++ sheet-12/1/a.s
@@ -3,51 +3,46 @@
 # for-loop:
 # a0 = 4
 # t0 = a0
-# t1 = 1
 #
 # do
+#   t1--
 #   a0 += t1
-#   t1++
-# while (t1!=t0)
+# while (t1!=0)
 # done
 
 # using if:
 # a0 = 4
 # t0 = a0
-# t1 = 1
 #
+# t1--
 # a0 += t1
-# t1++
-# if (t0!=t1)
-#   go to line 19
+# if (t0!=0)
+#   go to line 17
 # end if;
 
 
 # Assembly Code:
 
-# Precondition: save registers on stack, init vars a0, t0=a0
+# Precondition: save register(s) on stack, init vars a0=n, t0=a0
 # example: a0=4
 
 # Initialize stack pointer
 lui sp, 0x10000   # Set stack pointer to 0x10000000 (safe memory address), lower bits are automatically set to 0
 
-# save t0, t1 on stack
-addi sp, sp, -8
-sw t0, 4(sp)
-sw t1, 0(sp)
+# save t0 on stack
+addi sp, sp, -4
+sw t0, 0(sp)
 
-# start of loop
-addi a0, x0, 4 # n and result
-addi t0, a0, 0 # temp var that stores n
-addi t1, x0, 1 # counter
+addi a0, x0, 4 # init with n and stores result at the end
+addi t0, a0, 0 # temp var that counts down from n
 
 L1:
-add a0, a0, t1
-addi t1, t1, 1
-bne t1, t0, L1
+# start of loop
+addi t0, t0, -1
+add a0, a0, t0
+bne t0, x0, L1
 # end of loop
 
-# restore t0, t1 from stack
-lw t1, 0(sp)
-lw t0, 4(sp)
-addi sp, sp, 8 # free space on stack
+# restore t0 from stack
+lw t0, 0(sp)
+addi sp, sp, 4 # free space on stack
