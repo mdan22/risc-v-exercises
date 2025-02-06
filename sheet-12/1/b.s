b601e039452d9dd22f77960e3c58e787d8d83e06
mdan22 <mdan22@proton.me>
2025-02-06 12:15:21 +0100

    optimize 12-1-b

--- sheet-12/1/b.s
+++ sheet-12/1/b.s
@@ -17,48 +17,37 @@
 
 # Assembly Code:
 
-# Precondition: save registers on stack, init vars a0, t0=a0
-# example: a0=4
+# nested calculator of triangular numbers
+# Precondition: sw reg on stack, init var a0=4, t0=a0, t1=2
+
+tri_num_nested:
 
 # Initialize stack pointer
 lui sp, 0x10000   # Set stack pointer to 0x10000000 (safe memory address), lower bits are automatically set to 0
 
-# function trinum:
-addi a0, x0, 4 # n and result
-addi t2, x0, 2 # amount of times trinum is called
-addi t3, x0, 0 # counter 
-
-# save t2, t3 on stack
-addi sp, sp, -8
-sw t2, 4(sp)
-sw t3, 0(sp)
-
-
-L1:
 # save t0, t1 on stack
 addi sp, sp, -8
 sw t0, 4(sp)
 sw t1, 0(sp)
 
-# start of loop
-addi t0, a0, 0 # temp var that stores n
-addi t1, x0, 1 # counter
+# init a0, t0, t1
+addi a0, x0, 4 # a0 = n and a0 stores result at end
+addi t1, x0, 2 # t1 = nestings
 
-L2:
-add a0, a0, t1
-addi t1, t1, 1
-bne t1, t0, L2
-# end of loop
+nestings_loop:
+  addi t0, a0, 0 # t0 = a0, needs to be set each nesting
+
+  tri_num_loop:
+    addi t0, t0, -1
+    add a0, a0, t0
+    bne t0, x0, tri_num_loop
+  # end tri_num_loop
+
+  addi t1, t1, -1
+  bne t1, x0, nestings_loop
+# end nestings_loop
 
 # restore t0, t1 from stack
 lw t1, 0(sp)
 lw t0, 4(sp)
-addi sp, sp, 8 # free space on stack
-
-addi t3, t3, 1
-blt t3, t2, L1
-
-# restore t2, t3 from stack
-lw t3, 0(sp)
-lw t2, 4(sp)
 addi sp, sp, 8 # free space on stack
