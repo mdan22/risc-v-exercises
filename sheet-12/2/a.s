e207de5b77ee8043dc8f34b62aeac95db05a220e
mdan22 <mdan22@proton.me>
2025-02-07 10:02:57 +0100

    optimize 12-2-a

--- sheet-12/2/a.s
+++ sheet-12/2/a.s
@@ -1,66 +1,73 @@
 # ETI-detector (Pseudocode):
 
-# init base_address, counter:
-#   base_address = 0x10000000
-#   counter = 0
-#   
-# start:
-#   current_char = memory[base_address]
-#   if current_char == null:
-#       end program
-#   
+# eti_detector(memory[]) {
+#   // init index, character, matches:
+#   index = 0 // can be implemented as stack pointer
+#   index--
+#   char character;
+#   int matches = 0
+
 #   // Check for sequence E->T->I
-#   if current_char == 'E':
-#       next_char = memory[base_address + 1]
-#       if next_char == 'T':
-#           next_next_char = memory[base_address + 2] 
-#           if next_next_char == 'I':
-#               counter++
-#   
-#   base_address++
-#   goto start
-#
-# end:
-#   return counter
+#   while (true) {
+#     load_next_char();
+#     if (character == 'E') {
+#       load_next_char();
+#       if (character == 'T') {
+#         load_next_char();
+#         if (character == 'I') {
+#           matches++
+#         }
+#       }
+#     }
+#   }
+
+#   // before each check we load the next character
+#   load_next_char() {
+#     index++
+#     character = memory[index]
+#     if (current_char == null) {
+#       return matches
+#     }
+#   }
+# }
 
 
 # ETI-detector (Assembly Code):
+# sp is used as stack pointer
+# t0 is used to load a character
+# t1 is used to store E, T, I so t0 can be compared to it
 
 # init stack pointer to 256 (use hex for addresses)
-lui t1, 0x10000    # Set memory base address 0x10000000
-# set match counter to 0
-addi a0, x0, 0
-# will use t2 to compare current character
+lui sp, 0x10000    # Set memory base address 0x1000 0000
 
-start:
-lb t0, 0(t1)
-beq t0, x0, end
+# at cost of this line we can use load_next_char more often
+addi sp, sp, -1
+# set match matches to 0
+addi a0, x0, 0
+
+eti_detector:
 
 check_e:
-addi t2, x0, 69
-beq t0, t2, check_t
-jal x0, next_char
+jal ra, load_next_char
+addi t1, x0, 69
+beq t0, t1, check_t
 
 check_t:
-addi t1, t1, 1  # Move to next character
-lb t0, 0(t1)    # Load next character
-addi t2, x0, 84
-beq t0, t2, check_i
-jal x0, next_char
+jal ra, load_next_char
+addi t1, x0, 84
+beq t0, t1, check_i
 
 check_i:
-addi t1, t1, 1  # Move to next character
-lb t0, 0(t1)    # Load next character
-addi t2, x0, 73
-beq t0, t2, found_eti
-jal x0, next_char
-
-found_eti:
+jal ra, load_next_char
+addi t1, x0, 73
+bne t0, t1, check_e
 addi a0, a0, 1
 
-next_char:
-ADDI t1, t1, 1
-jal x0, start
+load_next_char:
+ADDI sp, sp, 1
+lb t0, 0(sp)
+beq t0, x0, end
+jalr x0, ra, 0
 
 end:
 # end of program, a0 contains amount of ETI occurances
