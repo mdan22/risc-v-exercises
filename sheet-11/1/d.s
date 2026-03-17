8d5a2201743bf3ac8fd979b22b50a937972fe78c
mdan22 <mdan22@proton.me>
2026-03-17 13:10:29 +0100

    remove exercise folders from main (moved to wise-2024-25 branch)

--- sheet-11/1/d.s
+++
@@ -1,1 +0,0 @@
-# Die Varianten aus b und c sind bereits sehr schnell. Je nachdem wie die Instruktionen OR und AND intern realisiert sind, könnte man damit noch schnellere Varianten implementieren. Man würde jedoch vermutlich mehr Register benötigen, um die Zwischenergebnisse zu speichern.
